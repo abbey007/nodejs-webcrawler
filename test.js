413185ba01b1f793d9e3b05fe81ecb1dc272f6b7
unknown <abeey007@gmail.com>
2023-10-04 19:54:06 +0530

    adding some changes

--- test.js
+++ test.js
@@ -3,8 +3,17 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 let resultArr = []
 
+const axiosInstance = axios.create();
+axiosInstance.defaults.maxRedirects = 10000;
+
+function removeDuplicates(arr) {
+    return [...new Set(arr)];
+}
+
 async function fetchHTML(url, depth) {
-    const { data } = await axios.get(url);
+
+    const { data } = await axiosInstance.get(url, { maxRedirects: 10000} );
+
     const $ = cheerio.load(data);
     const links = $("a")
     const images = $("img")
@@ -12,6 +21,8 @@ async function fetchHTML(url, depth) {
 
     links.each((index, value) => {
         let link = $(value).attr("href");
+        //let bgImg = $.attr("style")
+        //console.log(bgImg)
         if (link && link.indexOf("https") > -1 && link.indexOf("addtoany") == -1)
             arr.push($(value).attr("href"))
     })
@@ -25,24 +36,34 @@ async function fetchHTML(url, depth) {
         }
         resultArr.push(json);
     })
-   
-    for (let key in arr) {
-        if (depth > 0){
-            console.log(arr[key])
-            await fetchHTML(arr[key], depth - 1)
+    let filterArr = removeDuplicates(arr);
+    for (let key in filterArr) {
+        try {
+            if (depth > 0) {
+                console.log(arr[key])
+                await fetchHTML(arr[key], depth - 1)
+            }
+        } catch (error) {
+            console.log(error)
+            await fetchHTML(arr[+key+1], depth - 1)
         }
     }
 
 }
 
 async function asyncCall(link, depth) {
-    //console.log(process.argv)
-    await fetchHTML(link, parseInt(depth))
-    fs.writeFile("result.json", JSON.stringify(resultArr, null, 4), function (err) {
-        if (err) throw err;
-        console.log('complete');
+    try {
+        await fetchHTML(link, parseInt(depth))
+        fs.writeFile("result.json", JSON.stringify(resultArr, null, 4), function (err) {
+            if (err) throw err;
+            console.log('complete');
+        }
+        );
+    } catch (error) {
+        console.error(error);
     }
-    );
+
+
 }
 asyncCall(process.argv[2], process.argv[3])
 
